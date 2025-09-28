0f2d0a76c59ca9c3d8a5ebf5117e30760a082207
VijayentKohli <vijayentk.bits@gmail.com>
2025-09-28 12:44:35 -0500

    Adding score explanation

--- static/constants.js
+++ static/constants.js
@@ -7,7 +7,20 @@ const OUTPUT_DISPLAY_NAMES = {
     'risk_score_t': 'Risk Score'
 };
 
+// Score explanations - constant lines explaining what each score means
+const SCORE_EXPLANATIONS = {
+    'cost_t': 'Total medical expenditures in USD',
+    'bps_mean_t': 'Mean systolic blood pressure',
+    'gagne_sum_t': 'Total number of active chronic illnesses',
+    'ldl_mean_t': 'Mean low-density lipoprotein'
+};
+
 // Utility function to get display name
 function getOutputDisplayName(variableName) {
     return OUTPUT_DISPLAY_NAMES[variableName] || variableName;
+}
+
+// Simple function to get score explanation
+function getScoreExplanation(variableName) {
+    return SCORE_EXPLANATIONS[variableName] || '';
 }
